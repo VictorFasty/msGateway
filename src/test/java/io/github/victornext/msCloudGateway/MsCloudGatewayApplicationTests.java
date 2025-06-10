4f669235cc4e719c8dae4436f189dbdf292b9162
victornext <victorbrs517@gmail.com>
2025-06-09 23:33:13 -0300

    gateway

---
+++ src/test/java/io/github/victornext/msCloudGateway/MsCloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.victornext.msCloudGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
