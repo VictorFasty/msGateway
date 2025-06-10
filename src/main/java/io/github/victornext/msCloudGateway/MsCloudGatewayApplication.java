fb9cf5726e75d073de8424034544c8210fd498ec
victornext <victorbrs517@gmail.com>
2025-06-10 16:31:30 -0300

    implementação de mscards no gateway

--- src/main/java/io/github/victornext/msCloudGateway/MsCloudGatewayApplication.java
+++ src/main/java/io/github/victornext/msCloudGateway/MsCloudGatewayApplication.java
@@ -22,6 +22,7 @@ public class MsCloudGatewayApplication {
 		return builder
 				.routes()
 					.route(r -> r.path("/clients/**").uri("lb://msclients") )
+					.route(r -> r.path("/cards/**").uri("lb://mscards"))
 				.build();
 	}
 }
